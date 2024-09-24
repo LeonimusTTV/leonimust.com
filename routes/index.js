8d2cf8b67920a46018d523facaa166cb4f43e2a0
Leo <65367018+LeonimusTTV@users.noreply.github.com>
2024-09-24 22:26:58 +0200

    added games, removed all.css useless

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+
+const subdomains = ["https://supercold.leonimust.com", "https://doom.lenonimust.com"];
 
 router.get('/', function(req, res, next) {
   res.render('index', {isMainPage: true});
@@ -9,5 +12,23 @@ router.get('/projects', function(req, res, next) {
   res.render('projects', {isMainPage: false});
 });
 
+const checkSubdomain = async (url) => {
+  try {
+    const response = await axios.get(url, { timeout: 5000 });
+    return { url, status: response.status, statusText: 'Up' };
+  } catch (error) {
+    if (error.response) {
+      return { url, status: error.response.status, statusText: 'Down' };
+    } else {
+      return { url, status: 'No response', statusText: 'Down' };
+    }
+  }
+};
+
+// TODO - edit games and projects to now make a weird loading thing - me :3
+router.get('/games', async function(req, res, next) {
+  const results = await Promise.all(subdomains.map(checkSubdomain));
+  res.render('games', {isMainPage: false, results});
+});
 
 module.exports = router;
