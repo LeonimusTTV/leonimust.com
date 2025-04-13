d89759451b60df303000145b0420bc55b2e536dd
LeonimusTTV <leonimustytb@gmail.com>
2025-04-13 22:31:15 +0200

    feat: new project

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('layout', 'layout');
 
 function getIPv4FromIPv6(ip) {
   if (ip.startsWith('::ffff:')) {
-      return ip.replace('::ffff:', '');
+    return ip.replace('::ffff:', '');
   }
   return ip;
 }
@@ -28,7 +28,7 @@ function getIPv4FromIPv6(ip) {
 // see which ip is comming into the website
 app.use((req, res, next) => {
   let clientIp = req.clientIp;
-  
+
   console.log('Raw Client IP:', clientIp);
 
   next();
@@ -47,12 +47,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
